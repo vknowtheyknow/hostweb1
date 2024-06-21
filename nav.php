b6b972d8640e8441b0493ee2c0ea41bd47a404f5
Patchaa_GIT <56762841+vknowtheyknow@users.noreply.github.com>
2024-06-21 09:22:18 +0700

    1

---
+++ nav.php
@@ -0,0 +1,45 @@
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              CONAN
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  ">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="manga.php"> Manga</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="tv_series.php">TV series</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="movies.php">The movies</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="team.php">About us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"> <i class="fa fa-user" aria-hidden="true"></i> Login</a>
+              </li>
+              <form class="form-inline">
+                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+              </form>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
